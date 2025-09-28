5404e4c73fb1ede1e2cee88909246eecf2c9c1e5
jineth b <jineth200655@gmail.com>
2025-09-28 11:54:38 +0530

    added javascript
    javascript is step step by process

---
+++ index.js
@@ -0,0 +1,23 @@
+
+//btn.addEventListener('click',(event)=>{});    
+const list =new Array();    
+function addtask(){
+    const addtext=document.getElementById("addtask");
+    const todolist=document.getElementById('todolist');
+    const diverror=document.getElementById('errormsg');
+    if(addtext.value.trim()===""){
+        diverror.innerText="Please enter the task before adding!";
+        return;
+    }
+    diverror.innerText="";
+    console.log(addtext.value);  // to collect the data from the input to use the .value otherwise use the .textContent
+        list.push(addtext.value);
+        todolist.innerHTML+=`<li id="listed"><button onclick="complete(this)">${addtext.value}</button></li>`;
+        addtext.value='';
+    }        
+function complete(id){
+    const donelist=document.getElementById('Donelist');
+    donelist.innerHTML+=`<li>${id.textContent}</li>`;
+    id.parentElement.remove();
+    }
+     
